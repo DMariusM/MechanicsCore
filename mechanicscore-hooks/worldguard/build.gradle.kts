501b10e31ba058582e8b1ef1f8fe3696f05ff8e2
Collin Barber <collinjbarber@gmail.com>
2026-05-27 20:59:02 -0400

    fix: worldedit/worldguard pulling in conflicting dependencies

--- mechanicscore-hooks/worldguard/build.gradle.kts
+++ mechanicscore-hooks/worldguard/build.gradle.kts
@@ -5,6 +5,14 @@ plugins {
 dependencies {
     compileOnly(project(":mechanicscore-core"))
     compileOnly(libs.paper)
-    compileOnly(libs.worldedit)
-    compileOnly(libs.worldguard)
+    compileOnly(libs.worldedit) {
+        exclude(group = "com.google.guava", module = "guava")
+        exclude(group = "com.google.code.gson", module = "gson")
+        exclude(group = "it.unimi.dsi", module = "fastutil")
+    }
+    compileOnly(libs.worldguard) {
+        exclude(group = "com.google.guava", module = "guava")
+        exclude(group = "com.google.code.gson", module = "gson")
+        exclude(group = "it.unimi.dsi", module = "fastutil")
+    }
 }
